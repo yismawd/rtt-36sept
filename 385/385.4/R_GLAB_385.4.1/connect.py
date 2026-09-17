de1b578104270c65e6f02b2c51dbe39f8013d490
yismawd <ytsigie@gmail.com>
2026-09-17 12:09:50 -0500

    update

---
+++ 385/385.4/R_GLAB_385.4.1/connect.py
@@ -0,0 +1,29 @@
+# To connect to MySQL, we need mysql-connector-python
+import mysql.connector as mydbconnection # using an alias for clarity
+from mysql.connector import Error # Error function for special MySQL errors
+
+def connect():
+    conn = None
+
+    try:
+        conn = mydbconnection.connect(
+            database='classicmodels',
+            user='root',
+            password='password' # password for mysql server
+        )
+
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(f'❌ Error: {e}')
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+            print('Connection Closed')
+
+
+
+if __name__ == "__main__":
+    connect()
